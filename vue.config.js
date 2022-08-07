66ffd2655e05e9f1548063843520652a9edfb02b
UrakFerraz <urakmkt@yahoo.com.br>
2022-08-07 14:35:58 -0300

    configura sourcemap cssem vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,17 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
     transpileDependencies: true,
+
     pwa: {
         name: "todo-ts",
     },
+
     configureWebpack: {
         devtool: "source-map",
+    },
+
+    css: {
+        sourceMap: true,
     },
     devServer: {
         port: 3000,
