8ff29bcdcc37e0fe73c4dcdfed9808ed5975433c
UrakFerraz <urakmkt@yahoo.com.br>
2022-08-08 00:47:33 -0300

    adiciona avatar no state

--- src/store/user.ts
+++ src/store/user.ts
@@ -1,15 +1,21 @@
 import { User } from '@supabase/supabase-js'
 import { defineStore } from 'pinia'
 
-type AuthUserState = { user: User | null }
+type AuthUserState = { user: User | null; avatarURL: string }
 
 export const userStore = defineStore('user', {
     state: (): AuthUserState => {
         return {
             user: null,
+            avatarURL: '../assets/no_image_available.jpeg',
         }
     },
     getters: {
-        mainUser: (state) => state.user,
+        mainState: (state) => state,
+    },
+    actions: {
+        setAvatarURL(url: string) {
+            this.avatarURL = url
+        },
     },
 })
